2b733a89fd78f17387ea1cbffac89efb24946ec4
Maxym Malyshenko <qpace228@gmail.com>
2024-05-23 17:46:10 +0300

    commit2

---
+++ tgbot/Logic/LogicTextCLass1a.cs
@@ -0,0 +1,10 @@
+﻿namespace tgbot.Logic
+{
+    internal class LogicTextCLass1a : LogicTextClass
+    {
+        public override string LogicText(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- tgbot/Logic/LogicTextClass.cs
+++ tgbot/Logic/LogicTextClass.cs
@@ -2,7 +2,7 @@
 {
     internal class LogicTextClass : ILogicText
     {
-        public string LogicText(string data)
+        public virtual string LogicText(string data)
         {
             throw new NotImplementedException();
             //обработка полученных данных
